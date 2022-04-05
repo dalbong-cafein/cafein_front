c58f00df9b18529bcf329141e6ad8c3048e675b6
여정화 <22sonamu@naver.com>
2022-04-05 22:33:50 +0900

    bottom navigation

--- lib/Main/MainScreen.dart
+++ lib/Main/MainScreen.dart
@@ -9,11 +9,22 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int currentIndex = 0 ; //TODO like fragment
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: _bottomnavigation(),
-      body: Container(),
+      body: IndexedStack(
+        index: currentIndex, //TODO 바텀네비게이션뷰 선택하면 숫자 바뀌도록함
+       children: [
+          _MainWidget(),
+          _MainWidget2(),
+          _MainWidget3(),
+          _MainWidget4()
+        ],
+
+      ),
 
     );
   }
@@ -22,9 +33,10 @@ class _MainScreenState extends State<MainScreen> {
   Widget _bottomnavigation(){
     return BottomNavigationBar(
       fixedColor: Colors.black,
-      currentIndex: 1,
+      currentIndex: currentIndex,
       showSelectedLabels: false, //TODO 아이콘만 보이도록
       showUnselectedLabels: false,
+      onTap: onTabTapped,
       items: [
         BottomNavigationBarItem(
           icon : Icon(Icons.home),
@@ -48,6 +60,31 @@ class _MainScreenState extends State<MainScreen> {
 
     );
 
+  }
+  //TODO 클릭된 메뉴 index 반영
+  void onTabTapped(int index) { setState(() { currentIndex = index; }); }
+
+
+  Widget _MainWidget(){
+    return Container(
+      
+      child: Text("1"),
+    );
+  }
+  Widget _MainWidget2(){
+    return Container(
+      child: Text("2"),
+    );
+  }
+  Widget _MainWidget3(){
+    return Container(
+      child: Text("3"),
+    );
+  }
+  Widget _MainWidget4(){
+    return Container(
+      child: Text("4"),
+    );
   }
 
 
