f9a08f0371d61756e3349e0bea21b96097c32fad
여정화 <22sonamu@naver.com>
2022-04-05 23:42:09 +0900

    mainscreen

--- lib/Main/MainScreen.dart
+++ lib/Main/MainScreen.dart
@@ -11,14 +11,17 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int currentIndex = 0 ; //TODO like fragment
 
+
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width ;
     return Scaffold(
       bottomNavigationBar: _bottomnavigation(),
       body: IndexedStack(
         index: currentIndex, //TODO 바텀네비게이션뷰 선택하면 숫자 바뀌도록함
        children: [
-          _MainWidget(),
+          _MainWidget(height , width),
           _MainWidget2(),
           _MainWidget3(),
           _MainWidget4()
@@ -33,6 +36,7 @@ class _MainScreenState extends State<MainScreen> {
   Widget _bottomnavigation(){
     return BottomNavigationBar(
       fixedColor: Colors.black,
+      unselectedItemColor: Color(0xffACACAC),
       currentIndex: currentIndex,
       showSelectedLabels: false, //TODO 아이콘만 보이도록
       showUnselectedLabels: false,
@@ -43,15 +47,15 @@ class _MainScreenState extends State<MainScreen> {
           label: "hello"
         ),
         BottomNavigationBarItem(
-          icon : Icon(Icons.home),
+          icon : Icon(Icons.search),
             label: "hello"
         ),
         BottomNavigationBarItem(
-          icon : Icon(Icons.home),
+          icon : Icon(Icons.notifications),
             label: "hello"
         ),
         BottomNavigationBarItem(
-          icon : Icon(Icons.home),
+          icon : Icon(Icons.more_horiz),
             label: "hello"
         ),
 
@@ -65,11 +69,52 @@ class _MainScreenState extends State<MainScreen> {
   void onTabTapped(int index) { setState(() { currentIndex = index; }); }
 
 
-  Widget _MainWidget(){
-    return Container(
-      
-      child: Text("1"),
+  Widget _MainWidget(double height , double width){
+    return Scaffold(
+      appBar: AppBar(title: Text("Cafein", style: TextStyle(color: Color(0xffACACAC)),),backgroundColor: Colors.white,),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(left: width * 0.05),
+            child: Row(
+              children: [
+                Container(
+                  width: width * 0.15,
+                  height: height * 0.15,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage("imgs/appimg.png")
+                    )
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top : height * 0.01, bottom: height * 0.02, left: width * 0.05),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(bottom: height * 0.01),
+                        child: Row(
+                          children: [
+                            Text("카페인", style: TextStyle(fontSize: 20, fontWeight:FontWeight.bold ),),
+                            Icon(Icons.chevron_right, color: Color(0xffACACAC),)
+                          ],
+                        ),
+                      ),
+                      Text("카페인 1 일차", style: TextStyle(color: Color(0xffACACAC),fontWeight: FontWeight.normal),)
+                    ],
+                  ),
+                )
+
+
+              ],
+            ),
+          )
+        ],
+
+      )
     );
+
   }
   Widget _MainWidget2(){
     return Container(
