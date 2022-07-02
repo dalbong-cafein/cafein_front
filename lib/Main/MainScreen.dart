a27ee7adf2d561aa4116de9455cf63d88bbb74cf
22sonamu <22sonamu@naver.com>
2022-07-02 20:09:49 +0900

    mainscreen

--- lib/Main/MainScreen.dart
+++ lib/Main/MainScreen.dart
@@ -2037,7 +2037,8 @@ class _MainScreenState extends State<MainScreen> {
                 ),
                 Padding(
                   padding: EdgeInsets.only(top : 12 * h_percent),
-                  child: _cafeSearchList(h_percent, w_percent),
+                  child: _cafeSearchList(h_percent, w_percent)
+                    
                 ),
               ],
             ),
@@ -2062,7 +2063,16 @@ class _MainScreenState extends State<MainScreen> {
           offset: Offset(0, -10), // changes position of shadow
         ),
       ],
-      panel: _cafeList(h_percent, w_percent),
+      panelBuilder: (ScrollController sc) => ListView.separated(
+        controller: sc,
+        itemCount: 5,
+        itemBuilder: (BuildContext context, int index){
+          return _cafeListOne(h_percent, w_percent, index);
+        },
+        separatorBuilder: (BuildContext context, int index){
+          return Container();
+        },
+      ),
       header: Container(
         width: w_percent * width_whole,
         child: Row(
@@ -2086,27 +2096,6 @@ class _MainScreenState extends State<MainScreen> {
             )
           ],
         ),
-      ),
-    );
-  }
-
-  Widget _cafeList(double h_percent, double w_percent){
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        width : w_percent * 328 ,
-        height: 331 * h_percent,
-        color : Colors.green,
-        child: ListView(
-
-          children: [
-            _cafeListOne(h_percent, w_percent, 0),
-            _cafeListOne(h_percent, w_percent, 1),
-            _cafeListOne(h_percent, w_percent, 2),
-
-
-          ],
-        )
       ),
     );
   }
@@ -2136,7 +2125,7 @@ class _MainScreenState extends State<MainScreen> {
                         borderRadius: BorderRadius.circular(8), // Image border
                         child: SizedBox.fromSize(
                           size: Size.fromRadius(48), // Image radius
-                          child: Image.network(searchCafes[index]['storeImageDto']?['imageUrl'] != null ?searchCafes[index]['storeImageDto']['imageUrl'] : 'https://picsum.photos/250?image=11', fit: BoxFit.cover),
+                          child: Image.network('https://picsum.photos/250?image=11'),
                         ),
                       )
                   ),
@@ -2149,12 +2138,13 @@ class _MainScreenState extends State<MainScreen> {
                       Container(
                         width: 242 * w_percent,
                         height: 16 *h_percent,
-                        child: Text(searchCafes[index]['storeName'],  style: TextStyle(fontSize: 15, fontFamily: 'MainFont', fontWeight: FontWeight.w600),),
+                        child: Text("안녕",  style: TextStyle(fontSize: 15, fontFamily: 'MainFont', fontWeight: FontWeight.w600),),
                       ),
 
 
                     ],
                   ),
+                  
                 )
 
               ],
